92a6f505560158ff51fa03a5e7cf3eeff08ea812
KitayamaRosanjin <avignon08546@icloud.com>
2025-10-15 01:35:59 +0900

    Fix Heroku DisallowedHost: Add '*' to ALLOWED_HOSTS

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-fm+=&s#f&z319h1^u#whmaw(zsh@hi&8%-9vmk%)dtho+pa17-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
